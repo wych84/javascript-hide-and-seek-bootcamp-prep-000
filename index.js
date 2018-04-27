f82e46481d80568c68ef1b113282f263ed4efd48
Joyce Chun <wooyangee@gmail.com>
2018-04-27 23:17:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget () {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy (n) {
+  var rank = document.querySelectorAll (`.ranked-list`);
+  for (var i=0; i<rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var node = document.getElementById(`grand-node`);
+  var nextNode = node.children[0];
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  return node;
+}

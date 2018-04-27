8d13d283c0b7e8e3a5d2f50b523190f2f53ad5e7
Joyce Chun <wooyangee@gmail.com>
2018-04-27 23:07:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget () {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy (n) {
+  var rank = document.querySelectorAll (`.ranked-list`);
+  for (var i=0; i<rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  
+}
